dd3f1a9e479565c7b92c5ace36927d2d1bc08756
akki1027 <akki10271995@gmail.com>
2020-03-13 14:16:47 +0000

    Fixed

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,74 +1,55 @@
 class PostImagesController < ApplicationController
-  before_action :set_post_image, only: [:show, :edit, :update, :destroy]
-
-  # GET /post_images
-  # GET /post_images.json
+  before_action :correct_user, only: [:edit, :update]
   def index
     @post_images = PostImage.all
+    @user = current_user
   end
 
-  # GET /post_images/1
-  # GET /post_images/1.json
   def show
+    @post_images = PostImage.find(params[:id])
   end
 
-  # GET /post_images/new
   def new
     @post_image = PostImage.new
   end
 
-  # GET /post_images/1/edit
   def edit
+    post_images = PostImage.find(params[:id])
   end
 
-  # POST /post_images
-  # POST /post_images.json
+  def update
+    post_images = PostImage.find(params[:id])
+    post_image.update(post_image_params)
+    redirect_to post_images_path
+  end
+
   def create
     @post_image = PostImage.new(post_image_params)
-
-    respond_to do |format|
-      if @post_image.save
-        format.html { redirect_to @post_image, notice: 'Post image was successfully created.' }
-        format.json { render :show, status: :created, location: @post_image }
-      else
-        format.html { render :new }
-        format.json { render json: @post_image.errors, status: :unprocessable_entity }
-      end
+    @post_image.user_id = current_user.id
+    if @book.save
+      flash[:create] = "You have posted a photo successfully."
+      redirect_to post_image_path(@book)
+    else
+      @post_image = PostImage.all
+      @user = current_user
+      render 'index'
     end
   end
 
-  # PATCH/PUT /post_images/1
-  # PATCH/PUT /post_images/1.json
-  def update
-    respond_to do |format|
-      if @post_image.update(post_image_params)
-        format.html { redirect_to @post_image, notice: 'Post image was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post_image }
-      else
-        format.html { render :edit }
-        format.json { render json: @post_image.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /post_images/1
-  # DELETE /post_images/1.json
   def destroy
-    @post_image.destroy
-    respond_to do |format|
-      format.html { redirect_to post_images_url, notice: 'Post image was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    post_images = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_image_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post_image
-      @post_image = PostImage.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
     def post_image_params
       params.require(:post_image).permit(:image_id, :caption, :location, :user_id)
     end
+    def correct_user
+    @book = Book.find(params[:id])
+    if current_user != @post_image.user
+      redirect_to post_images_path
+    end
+  end
 end
