f9f252fabe1531aba9a18deee82048918a4e365c
akki1027 <akki10271995@gmail.com>
2020-03-15 01:28:17 +0000

    Added favorties

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,8 @@ class PostImage < ApplicationRecord
 	attachment :image
 	validates :image, presence: true
 	has_many :post_comments, dependent: :destroy
+	has_many :favorites, dependent: :destroy
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :post_images, only:[:new, :create, :index, :show, :edit] do
-  	resource :post_comments, only:[:create]
+  	resource :post_comments, only:[:create, :destroy]
+  	resource :favorites, only:[:create, :destroy]
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
